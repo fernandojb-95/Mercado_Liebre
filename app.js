36018be66d5ec66bdb6599e22a0269d1360e4e0e
fernandojb-95 <fernando.bjuarez95@gmail.com>
2021-08-17 17:52:01 -0500

    Se agrega Procfile y se ajusta el puerto

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT;
 app.use(express.static(path.resolve(__dirname , './public')))
-app.listen(3000, () => {
-    console.log("Server running at port 3000")
-});
+app.listen(port || 3000, () => {
+    console.log(`Server running at port ${port || 3000}`)
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
